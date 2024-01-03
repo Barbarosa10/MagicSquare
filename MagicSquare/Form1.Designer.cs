0d8895a8fa6ae67cae3e3945c6d999721b0e44aa
Tiberiu Amariuta <tiberiu.amariuta@student.tuiasi.ro>
2024-01-03 10:50:41 +0200

    interface

---
+++ MagicSquare/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace MagicSquare
+{
+    partial class FormMagicSquare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.el11 = new System.Windows.Forms.TextBox();
+            this.el12 = new System.Windows.Forms.TextBox();
+            this.el13 = new System.Windows.Forms.TextBox();
+            this.el14 = new System.Windows.Forms.TextBox();
+            this.el15 = new System.Windows.Forms.TextBox();
+            this.el21 = new System.Windows.Forms.TextBox();
+            this.el31 = new System.Windows.Forms.TextBox();
+            this.el41 = new System.Windows.Forms.TextBox();
+            this.el51 = new System.Windows.Forms.TextBox();
+            this.el22 = new System.Windows.Forms.TextBox();
+            this.el23 = new System.Windows.Forms.TextBox();
+            this.el24 = new System.Windows.Forms.TextBox();
+            this.el25 = new System.Windows.Forms.TextBox();
+            this.el32 = new System.Windows.Forms.TextBox();
+            this.el33 = new System.Windows.Forms.TextBox();
+            this.el34 = new System.Windows.Forms.TextBox();
+            this.el35 = new System.Windows.Forms.TextBox();
+            this.el42 = new System.Windows.Forms.TextBox();
+            this.el43 = new System.Windows.Forms.TextBox();
+            this.el44 = new System.Windows.Forms.TextBox();
+            this.el45 = new System.Windows.Forms.TextBox();
+            this.el52 = new System.Windows.Forms.TextBox();
+            this.el53 = new System.Windows.Forms.TextBox();
+            this.el54 = new System.Windows.Forms.TextBox();
+            this.el55 = new System.Windows.Forms.TextBox();
+            this.Dimensiune = new System.Windows.Forms.Label();
+            this.comboDimensiune = new System.Windows.Forms.ComboBox();
+            this.Suma = new System.Windows.Forms.Label();
+            this.textSuma = new System.Windows.Forms.TextBox();
+            this.Rezolvare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(393, 54);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(127, 24);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Magic Square";
+            // 
+            // el11
+            // 
+            this.el11.Location = new System.Drawing.Point(105, 157);
+            this.el11.Name = "el11";
+            this.el11.Size = new System.Drawing.Size(30, 20);
+            this.el11.TabIndex = 1;
+            // 
+            // el12
+            // 
+            this.el12.Location = new System.Drawing.Point(172, 157);
+            this.el12.Name = "el12";
+            this.el12.Size = new System.Drawing.Size(30, 20);
+            this.el12.TabIndex = 2;
+            // 
+            // el13
+            // 
+            this.el13.Location = new System.Drawing.Point(239, 157);
+            this.el13.Name = "el13";
+            this.el13.Size = new System.Drawing.Size(30, 20);
+            this.el13.TabIndex = 3;
+            // 
+            // el14
+            // 
+            this.el14.Location = new System.Drawing.Point(305, 157);
+            this.el14.Name = "el14";
+            this.el14.Size = new System.Drawing.Size(30, 20);
+            this.el14.TabIndex = 4;
+            // 
+            // el15
+            // 
+            this.el15.Location = new System.Drawing.Point(371, 157);
+            this.el15.Name = "el15";
+            this.el15.Size = new System.Drawing.Size(30, 20);
+            this.el15.TabIndex = 5;
+            // 
+            // el21
+            // 
+            this.el21.Location = new System.Drawing.Point(105, 202);
+            this.el21.Name = "el21";
+            this.el21.Size = new System.Drawing.Size(30, 20);
+            this.el21.TabIndex = 6;
+            // 
+            // el31
+            // 
+            this.el31.Location = new System.Drawing.Point(105, 246);
+            this.el31.Name = "el31";
+            this.el31.Size = new System.Drawing.Size(30, 20);
+            this.el31.TabIndex = 7;
+            // 
+            // el41
+            // 
+            this.el41.Location = new System.Drawing.Point(105, 292);
+            this.el41.Name = "el41";
+            this.el41.Size = new System.Drawing.Size(30, 20);
+            this.el41.TabIndex = 8;
+            // 
+            // el51
+            // 
+            this.el51.Location = new System.Drawing.Point(105, 334);
+            this.el51.Name = "el51";
+            this.el51.Size = new System.Drawing.Size(30, 20);
+            this.el51.TabIndex = 9;
+            // 
+            // el22
+            // 
+            this.el22.Location = new System.Drawing.Point(172, 202);
+            this.el22.Name = "el22";
+            this.el22.Size = new System.Drawing.Size(30, 20);
+            this.el22.TabIndex = 10;
+            // 
+            // el23
+            // 
+            this.el23.Location = new System.Drawing.Point(239, 202);
+            this.el23.Name = "el23";
+            this.el23.Size = new System.Drawing.Size(30, 20);
+            this.el23.TabIndex = 11;
+            // 
+            // el24
+            // 
+            this.el24.Location = new System.Drawing.Point(305, 202);
+            this.el24.Name = "el24";
+            this.el24.Size = new System.Drawing.Size(30, 20);
+            this.el24.TabIndex = 12;
+            // 
+            // el25
+            // 
+            this.el25.Location = new System.Drawing.Point(371, 202);
+            this.el25.Name = "el25";
+            this.el25.Size = new System.Drawing.Size(30, 20);
+            this.el25.TabIndex = 13;
+            // 
+            // el32
+            // 
+            this.el32.Location = new System.Drawing.Point(172, 246);
+            this.el32.Name = "el32";
+            this.el32.Size = new System.Drawing.Size(30, 20);
+            this.el32.TabIndex = 14;
+            // 
+            // el33
+            // 
+            this.el33.Location = new System.Drawing.Point(239, 246);
+            this.el33.Name = "el33";
+            this.el33.Size = new System.Drawing.Size(30, 20);
+            this.el33.TabIndex = 15;
+            // 
+            // el34
+            // 
+            this.el34.Location = new System.Drawing.Point(305, 246);
+            this.el34.Name = "el34";
+            this.el34.Size = new System.Drawing.Size(30, 20);
+            this.el34.TabIndex = 16;
+            // 
+            // el35
+            // 
+            this.el35.Location = new System.Drawing.Point(371, 246);
+            this.el35.Name = "el35";
+            this.el35.Size = new System.Drawing.Size(30, 20);
+            this.el35.TabIndex = 17;
+            // 
+            // el42
+            // 
+            this.el42.Location = new System.Drawing.Point(172, 292);
+            this.el42.Name = "el42";
+            this.el42.Size = new System.Drawing.Size(30, 20);
+            this.el42.TabIndex = 18;
+            // 
+            // el43
+            // 
+            this.el43.Location = new System.Drawing.Point(239, 292);
+            this.el43.Name = "el43";
+            this.el43.Size = new System.Drawing.Size(30, 20);
+            this.el43.TabIndex = 19;
+            // 
+            // el44
+            // 
+            this.el44.Location = new System.Drawing.Point(305, 292);
+            this.el44.Name = "el44";
+            this.el44.Size = new System.Drawing.Size(30, 20);
+            this.el44.TabIndex = 20;
+            // 
+            // el45
+            // 
+            this.el45.Location = new System.Drawing.Point(371, 292);
+            this.el45.Name = "el45";
+            this.el45.Size = new System.Drawing.Size(30, 20);
+            this.el45.TabIndex = 21;
+            // 
+            // el52
+            // 
+            this.el52.Location = new System.Drawing.Point(172, 334);
+            this.el52.Name = "el52";
+            this.el52.Size = new System.Drawing.Size(30, 20);
+            this.el52.TabIndex = 22;
+            // 
+            // el53
+            // 
+            this.el53.Location = new System.Drawing.Point(239, 334);
+            this.el53.Name = "el53";
+            this.el53.Size = new System.Drawing.Size(30, 20);
+            this.el53.TabIndex = 23;
+            // 
+            // el54
+            // 
+            this.el54.Location = new System.Drawing.Point(305, 334);
+            this.el54.Name = "el54";
+            this.el54.Size = new System.Drawing.Size(30, 20);
+            this.el54.TabIndex = 24;
+            // 
+            // el55
+            // 
+            this.el55.Location = new System.Drawing.Point(371, 334);
+            this.el55.Name = "el55";
+            this.el55.Size = new System.Drawing.Size(30, 20);
+            this.el55.TabIndex = 25;
+            // 
+            // Dimensiune
+            // 
+            this.Dimensiune.AutoSize = true;
+            this.Dimensiune.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dimensiune.Location = new System.Drawing.Point(601, 154);
+            this.Dimensiune.Name = "Dimensiune";
+            this.Dimensiune.Size = new System.Drawing.Size(180, 16);
+            this.Dimensiune.TabIndex = 26;
+            this.Dimensiune.Text = "Alege dimensiunea patratului";
+            // 
+            // comboDimensiune
+            // 
+            this.comboDimensiune.FormattingEnabled = true;
+            this.comboDimensiune.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5"});
+            this.comboDimensiune.Location = new System.Drawing.Point(671, 189);
+            this.comboDimensiune.Name = "comboDimensiune";
+            this.comboDimensiune.Size = new System.Drawing.Size(42, 21);
+            this.comboDimensiune.TabIndex = 27;
+            // 
+            // Suma
+            // 
+            this.Suma.AutoSize = true;
+            this.Suma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Suma.Location = new System.Drawing.Point(629, 247);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(122, 16);
+            this.Suma.TabIndex = 29;
+            this.Suma.Text = "Alegeți suma dorită";
+            // 
+            // textSuma
+            // 
+            this.textSuma.Location = new System.Drawing.Point(668, 289);
+            this.textSuma.Name = "textSuma";
+            this.textSuma.Size = new System.Drawing.Size(45, 20);
+            this.textSuma.TabIndex = 30;
+            // 
+            // Rezolvare
+            // 
+            this.Rezolvare.Location = new System.Drawing.Point(626, 343);
+            this.Rezolvare.Name = "Rezolvare";
+            this.Rezolvare.Size = new System.Drawing.Size(125, 28);
+            this.Rezolvare.TabIndex = 31;
+            this.Rezolvare.Text = "Rezolvă problema";
+            this.Rezolvare.UseVisualStyleBackColor = true;
+            // 
+            // FormMagicSquare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 486);
+            this.Controls.Add(this.Rezolvare);
+            this.Controls.Add(this.textSuma);
+            this.Controls.Add(this.Suma);
+            this.Controls.Add(this.comboDimensiune);
+            this.Controls.Add(this.Dimensiune);
+            this.Controls.Add(this.el55);
+            this.Controls.Add(this.el54);
+            this.Controls.Add(this.el53);
+            this.Controls.Add(this.el52);
+            this.Controls.Add(this.el45);
+            this.Controls.Add(this.el44);
+            this.Controls.Add(this.el43);
+            this.Controls.Add(this.el42);
+            this.Controls.Add(this.el35);
+            this.Controls.Add(this.el34);
+            this.Controls.Add(this.el33);
+            this.Controls.Add(this.el32);
+            this.Controls.Add(this.el25);
+            this.Controls.Add(this.el24);
+            this.Controls.Add(this.el23);
+            this.Controls.Add(this.el22);
+            this.Controls.Add(this.el51);
+            this.Controls.Add(this.el41);
+            this.Controls.Add(this.el31);
+            this.Controls.Add(this.el21);
+            this.Controls.Add(this.el15);
+            this.Controls.Add(this.el14);
+            this.Controls.Add(this.el13);
+            this.Controls.Add(this.el12);
+            this.Controls.Add(this.el11);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "FormMagicSquare";
+            this.Text = "Magic Square";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.TextBox el11;
+        private System.Windows.Forms.TextBox el12;
+        private System.Windows.Forms.TextBox el13;
+        private System.Windows.Forms.TextBox el14;
+        private System.Windows.Forms.TextBox el15;
+        private System.Windows.Forms.TextBox el21;
+        private System.Windows.Forms.TextBox el31;
+        private System.Windows.Forms.TextBox el41;
+        private System.Windows.Forms.TextBox el51;
+        private System.Windows.Forms.TextBox el22;
+        private System.Windows.Forms.TextBox el23;
+        private System.Windows.Forms.TextBox el24;
+        private System.Windows.Forms.TextBox el25;
+        private System.Windows.Forms.TextBox el32;
+        private System.Windows.Forms.TextBox el33;
+        private System.Windows.Forms.TextBox el34;
+        private System.Windows.Forms.TextBox el35;
+        private System.Windows.Forms.TextBox el42;
+        private System.Windows.Forms.TextBox el43;
+        private System.Windows.Forms.TextBox el44;
+        private System.Windows.Forms.TextBox el45;
+        private System.Windows.Forms.TextBox el52;
+        private System.Windows.Forms.TextBox el53;
+        private System.Windows.Forms.TextBox el54;
+        private System.Windows.Forms.TextBox el55;
+        private System.Windows.Forms.Label Dimensiune;
+        private System.Windows.Forms.ComboBox comboDimensiune;
+        private System.Windows.Forms.Label Suma;
+        private System.Windows.Forms.TextBox textSuma;
+        private System.Windows.Forms.Button Rezolvare;
+    }
+}
+
